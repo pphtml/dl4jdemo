e3da029dbd858b80d9c124ed9cffd0d1ea2297eb
Petr Pravda <petr.pravda@gmail.com>
2018-03-04 16:54:35 +0100

    scheduler test

--- src/main/java/org/superbiz/web/MainScheduler.java
+++ src/main/java/org/superbiz/web/MainScheduler.java
@@ -1,9 +1,15 @@
 package org.superbiz.web;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import org.superbiz.fetch.FetchFinViz;
+import org.superbiz.guice.BasicModule;
 import org.superbiz.util.GlobalInit;
 
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MainScheduler {
@@ -14,7 +20,7 @@ public class MainScheduler {
     public static void main(String[] args) throws SchedulerException {
         SchedulerFactory schedulerFactory = new StdSchedulerFactory();
         Scheduler scheduler = schedulerFactory.getScheduler();
-        JobDetail job = JobBuilder.newJob(SimpleJob.class)
+        JobDetail job = JobBuilder.newJob(FetchFinVizJob.class)
                 .withIdentity("myJob", "group1")
                 .build();
 
@@ -32,7 +38,9 @@ public class MainScheduler {
 
         CronTrigger cronTrigger = TriggerBuilder.newTrigger()
                 .withIdentity("trigger3", "group1")
-                .withSchedule(CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 58 15 * * ?"))
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 59 15 * * ?"))
+                //.withSchedule(CronScheduleBuilder.cronSchedule("0/10 * * * * ?"))
                 //.withSchedule(CronScheduleBuilder.cronSchedule("0 0/1 * * * ?"))
                 //.forJob("myJob", "group1")
                 .build();
@@ -40,9 +48,20 @@ public class MainScheduler {
         scheduler.scheduleJob(job, cronTrigger);
     }
 
-    public static class SimpleJob implements Job {
+    public static class FetchFinVizJob implements Job {
         public void execute(JobExecutionContext arg0) throws JobExecutionException {
-            LOGGER.info("This is a quartz job!");
+            LOGGER.info("Starting");
+
+            try {
+                Injector injector = Guice.createInjector(new BasicModule());
+                FetchFinViz fetchData = injector.getInstance(FetchFinViz.class);
+                fetchData.fetchAll();
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE, e.getMessage(), e);
+                throw new JobExecutionException(e);
+            }
+
+            LOGGER.info("Ending");
         }
     }
 }
